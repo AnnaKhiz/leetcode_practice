30a123fc751c91f9466162cd122418cdc6486a91
annakhizhniak <kukurika.anna@gmail.com>
2025-01-08 19:38:09 +0200

    array transform done

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-var createCounter = function(init) {
-	let counter = init;
-    return {
-			increment() {
-				return ++counter;
-			},
-			decrement() {
-				return --counter;
-			},
-			reset() {
-				return counter = init;
-			}
-		}
+const arr = [10, 20, 30];
+const returnedArray = []
+
+var map = function(arr, fn) {
+	if (!arr.length) return [];
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr.length === 1 && arr[i] === 0) return [0];
+		returnedArray[i] = parseInt(fn(arr[i], i));
+	}
+	return returnedArray;
 };
 
-const counter = createCounter(5)
-console.log('increment', counter.increment()); // 6
-console.log('reset', counter.reset()) // 5
-console.log('decrement', counter.decrement()) // 4
+map(arr, plusI);
+
+function plusI(el, i) {
+	return el + i;
+}
+
+function plusone(n) {
+	return n + 1;
+}
+
+function constant() {
+	return parseInt(42);
+}
+
+function nMinusI(n, i) { 
+	return n - i;
+}
+
+function plus10(n) { 
+	return n + 10; 
+}
+function identity(n, i) { 
+	return n; 
+}
