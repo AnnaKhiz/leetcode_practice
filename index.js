90ecbe1fe92cecd7d50dc2d5842242b67006afcd
annakhizhniak <kukurika.anna@gmail.com>
2025-01-08 17:41:51 +0200

    simple counter done

--- index.js
+++ index.js
@@ -1,55 +1,19 @@
-var expect = function(val) {
-	return {
-		toBe(valueToBe) {
-			switch (true) {
-				case val instanceof Function || valueToBe instanceof Function:
-					return true
-				case !val && typeof val !== 'number' || !valueToBe && typeof valueToBe !== 'number' :
-					throw new Error('Not Equal')
-				case typeof val === 'string' || typeof valueToBe === 'string)':
-					if (Number.isNaN(val) || Number.isNaN(valueToBe)) {
-						if (val.localeCompare(valueToBe) !== 0) {
-							throw new Error('Not Equal')
-						} 
-						return true
-					} else {
-						if (val !== valueToBe) {
-							throw new Error('Not Equal')
-						} 
-						return true	
-					};
-				case  typeof val === 'object' || typeof valueToBe === 'object':
-						if (JSON.stringify(val) !== JSON.stringify(valueToBe) || !Object.is(val, valueToBe)) {
-							throw new Error('Not Equal')
-						} 
-						return true
-				default:
-						if (isNaN(val) || isNaN(valueToBe)) {
-							throw new Error('Not Equal')
-						}
-						return val === valueToBe
-				}
+var createCounter = function(init) {
+	let counter = init;
+    return {
+			increment() {
+				return ++counter;
 			},
-
-		notToBe(valueNotToBe) {
-			if (valueNotToBe !== null && val === valueNotToBe ) {
-				throw new Error('Equal')
+			decrement() {
+				return --counter;
+			},
+			reset() {
+				return counter = init;
 			}
-			return val !== valueNotToBe
 		}
-	}
 };
 
-//cases
-const o = {}; 
-expect(o).toBe(o)
-
-expect(5).toBe(5)
-expect('100%').toBe('50%')
-expect({}).notToBe({})
-expect(5).toBe(null)
-expect(5).notToBe(null)
-expect(NaN).notToBe(NaN)
-expect(Function).notToBe(Function)
-expect(true).notToBe(false)
-expect(0).notToBe(0)
+const counter = createCounter(5)
+console.log('increment', counter.increment()); // 6
+console.log('reset', counter.reset()) // 5
+console.log('decrement', counter.decrement()) // 4
