77baeb3cebd8e502eb2ae55f5b6293936901de1a
annakhizhniak <kukurika.anna@gmail.com>
2025-01-09 12:06:54 +0200

    filter array finished

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # leetcode_practice
 Small tasks from LeetCode for practice JS (30 Days of JavaScript)
 
-3 Task (easy):
+4 Task (easy):
 
-Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
 
-The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+The fn function takes one or two arguments:
 
-Please solve it without the built-in Array.map method.
+arr[i] - number from the arr
+i - index of arr[i]
+filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
+
+Please solve it without the built-in Array.filter method.
--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-const arr = [10, 20, 30];
-const returnedArray = []
+const arr = [-2,-1,0,1,2];
+var filter = function(arr, fn) {
+	let filteredArr = [];
 
-var map = function(arr, fn) {
 	if (!arr.length) return [];
-
 	for (let i = 0; i < arr.length; i++) {
+
 		if (arr.length === 1 && arr[i] === 0) return [0];
-		returnedArray[i] = parseInt(fn(arr[i], i));
+
+		if (fn(arr[i], i)) {
+			filteredArr.push(arr[i])
+		}
 	}
-	return returnedArray;
+	return filteredArr;
 };
 
-map(arr, plusI);
-
-function plusI(el, i) {
-	return el + i;
+function greaterThan10(n) {
+	return n > 10;
 }
 
-function plusone(n) {
-	return n + 1;
+function firstIndex(n, i) {
+	return i === 0;
 }
 
-function constant() {
-	return parseInt(42);
+function plusOne(n) {
+	return n + 1
 }
 
-function nMinusI(n, i) { 
-	return n - i;
-}
+console.log(filter(arr, greaterThan10))
+console.log(filter(arr, firstIndex))
+console.log(filter(arr, plusOne))
 
-function plus10(n) { 
-	return n + 10; 
-}
-function identity(n, i) { 
-	return n; 
-}
